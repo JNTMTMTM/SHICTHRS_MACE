e7cf8d5b8e28eea1002d643b6eabc995fa231b47
JNTMTMTM <15251865468@163.com>
2026-01-25 16:17:54 +0800

    V1.6.0 feat: 更新GPU信息获取方式并升级版本号
    将GPU信息从获取单个GPU改为获取所有GPU列表
    版本号从1.5.0升级至1.6.0

--- SHICTHRSMACE/SHICTHRSMACE/api/SHRMACE_get_gpu_info.py
+++ SHICTHRSMACE/SHICTHRSMACE/api/SHRMACE_get_gpu_info.py
@@ -7,6 +7,6 @@ def get_gpu_info(var) -> None:
     try:
         output = subprocess.check_output('wmic path win32_VideoController get name' , shell=True, text=True, stderr=subprocess.DEVNULL)
         gpus = [line.strip() for line in output.splitlines() if line.strip() and 'Name' not in line]
-        var.SHRMACEResult['GPUINFO'] = copy.deepcopy(gpus[-1] if gpus else "Unknown GPU")
+        var.SHRMACEResult['GPUINFO'] = copy.deepcopy(gpus)
     except Exception as e:
         raise SHRMACEException(f'SHRMACE [ERROR.2007] unable to get GPU info. | {''.join(e.args)}')
